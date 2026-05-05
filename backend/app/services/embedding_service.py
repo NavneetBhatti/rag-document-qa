064516e7fbfb78a9e481c58e2d2145916333d195
NavneetBhatti <navneetbhatti13@gmail.com>
2026-05-05 16:45:10 -0400

    create embedding service

---
+++ backend/app/services/embedding_service.py
@@ -0,0 +1,12 @@
+import google.generativeai as genai
+import os
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+def get_embedding(text: str):
+    response = genai.embed_content(
+        model="models/embedding-001",
+        content=text
+    )
+    return response["embedding"]
